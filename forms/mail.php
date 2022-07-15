dcb7b024407a1b0e3ffe5ced342d14170761e796
Noori16 <noorjahannazar786@gmail.com>
2022-07-15 15:21:00 +0530

    first commit

---
+++ forms/mail.php
@@ -0,0 +1,18 @@
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$subject = $_POST['subject'];
+$message= $_POST['message'];
+$to = "tasleemrahim09@mail.com";
+$subject = "Mail From R.A. ELECTRICALS AND WORKS";
+$txt ="Name = ". $name . "\r\n  Email = " . $email ."\r\nSubject = " .$subject . "\r\n Message =" . $message;
+$headers = "From: noreply@raelectrical.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
