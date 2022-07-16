2021a54079b9739e0200d234ea5ef43edb8ed778
Noori16 <109329611+Noori16@users.noreply.github.com>
2022-07-16 08:52:14 +0530

    Update mail.php

--- forms/mail.php
+++ forms/mail.php
@@ -6,7 +6,7 @@ $email= $_POST['email'];
 $subject = $_POST['subject'];
 $message= $_POST['message'];
 $to = "tasleemrahim09@mail.com";
-$subject = "Mail From R.A. ELECTRICALS AND WORKS";
+$subject = "Mail From NN ELECTRICALS AND WORKS";
 $txt ="Name = ". $name . "\r\n  Email = " . $email ."\r\nSubject = " .$subject . "\r\n Message =" . $message;
 $headers = "From: noreply@raelectrical.com" . "\r\n" .
 "CC: somebodyelse@example.com";
